4d0662ccc6ebaf868a211934edc0242c29fc6f49
Md Ajmal <maherukh1999@gmail.com>
2024-07-31 23:20:59 +0530

    Final project

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ let quote = document.querySelector("#text");
 let author = document.querySelector("#author");
 let button = document.querySelector("#new-quote");
 
-fetch('')
+function newquote() {
+fetch('https://api.quotable.io/random')
+.then(response => response.json())
+.then(data => {
+    quote.innerText = data.content;
+    author.innerText = "- " + data.author;
+})
+}
+newquote()
+button.addEventListener("click", newquote);
